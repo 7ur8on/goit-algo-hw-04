a9e435d847d0597b5afbadfa9211be39a680fd3d
7ur8on <7ur8on@gmail.com>
2024-04-27 13:12:41 +0200

    add cats info

---
+++ cats_info/main.py
@@ -0,0 +1,17 @@
+def get_cats_info(path):
+    cats_info = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                data = line.strip().split(',')
+                cat_info = {"id": data[0], "name": data[1], "age": data[2]}
+                cats_info.append(cat_info)
+    except FileNotFoundError:
+        print("Файл не знайдено!")
+    except Exception as e:
+        print("Сталася помилка:", e)
+    return cats_info
+
+# Приклад використання:
+cats_info = get_cats_info("cats_info/cats_file.txt")
+print(cats_info)
